e9fe16a611bdf1bd41e8fd15667e0e222d0f354e
miccraig15 <miccraig15@gmail.com>
2023-01-09 15:36:36 -0600

    Completed Part 3 and some final touches

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -9,7 +9,6 @@ export class FavPhotosComponent implements OnInit {
   photosTitle = 'My Favorite Photographs';
   image1 = 'https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-08/220805-border-collie-play-mn-1100-82d2f1.jpg';
   image2 = 'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2019/02/dog-451643.jpg?h=bf654dbc&itok=MQGvBmuo';
-  image3 = '';
 
   constructor() { }
 
